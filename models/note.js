0a2324ff445672b83c0ae50d133739a12e31e1d0
Sam Smiley <samtsmiley@gmail.com>
2018-11-27 12:37:25 -0800

    connected mongoose

--- models/note.js
+++ models/note.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const mongoose = require('mongoose');
 
@@ -9,5 +9,13 @@ const noteSchema = new mongoose.Schema({
 
 // Add `createdAt` and `updatedAt` fields
 noteSchema.set('timestamps', true);
+
+noteSchema.set('toJSON', {
+  virtuals: true,     // include built-in virtual `id`
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+    delete ret.__v;
+  }
+});
 
 module.exports = mongoose.model('Note', noteSchema);
