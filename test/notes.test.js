06b1595106d3dd895f7e0c33091a12213484f47e
Sam Smiley <samtsmiley@gmail.com>
2018-11-28 18:01:00 -0800

    alterd connect

--- test/notes.test.js
+++ test/notes.test.js
@@ -18,7 +18,7 @@ chai.use(chaiHttp);
 describe('Notes API resource', function() {
 
   before(function () {
-    return mongoose.connect(TEST_MONGODB_URI)
+    return mongoose.connect(TEST_MONGODB_URI, { useNewUrlParser: true })
       .then(() => mongoose.connection.db.dropDatabase());
   });
 
