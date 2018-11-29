037cde45bda118c667ad6589a705125a693bf470
Sam Smiley <samtsmiley@gmail.com>
2018-11-28 17:41:31 -0800

    added a bunch of test stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 
 const { PORT, MONGODB_URI } = require('./config');
@@ -44,18 +44,19 @@ app.use((err, req, res, next) => {
   }
 });
 
+if (require.main === module) {
 // Connect to DB and Listen for incoming connections
-mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error('\n === Did you remember to start `mongod`? === \n');
+  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
+
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
     console.error(err);
   });
-
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
-
+}
 module.exports = app; // Export for testing
